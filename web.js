3932c2fe429612c4127a7fa028ec52c86dbd0d3c
Naruto <97257063+luffy2011@users.noreply.github.com>
2022-06-09 21:04:49 +0100

    Add files via upload

--- web.js
+++ web.js
@@ -1,1 +1,86 @@
-let inpu = 
+let btnGet = document.querySelector('#btn-get');
+let btnSet = document.querySelector('#btn-set');
+let inputGet =  document.querySelector('#input-get');
+let inputSet =  document.querySelector('#input-set');
+let result = document.querySelector('#result');
+
+
+btnGet.addEventListener('click' , () =>{
+result.innerText = inputGet.value;
+alert('you have ordered , Please wait for 30 seconds food to come'),(200), 
+alert('just press okay')
+alert('  30 sec left ')
+alert('  29 sec left ')
+alert('  28 sec left ')
+alert('  27 sec left ')
+alert('  26 sec left ')
+alert('  25 sec left ')
+alert('  24 sec left ')
+alert('  23 sec left ')
+alert('  22 sec left ')
+alert('  21 sec left ')
+alert('  20 sec left ')
+alert('  19 sec left ')
+alert('  18 sec left ')
+alert('  17 sec left ')
+alert('  16 sec left ')
+alert('  15 sec left ')
+alert('  14 sec left ')
+alert('  13 sec left ')
+alert('  12 sec left ')
+alert('  11 sec left ')
+alert('  10 sec left ')
+alert('  9 sec left ')
+alert('  8 sec left ')
+alert('  7 sec left ')
+alert('  6 sec left ')
+alert('  5 sec left ')
+alert('  4 sec left ')
+alert('  3 sec left ')
+alert('  2 sec left ')
+alert('  1 sec left ')
+alert('Your order has arrived enjoy the tasty food')
+
+alert('thanks for eating here at all foods cafe')
+alert('please pay before you leave')
+prompt('pay here it 10 $/£')
+
+
+alert('bye hope you soon come back')
+
+
+
+
+
+
+});
+
+
+btnSet.addEventListener('click' , () => {
+  inputGet.value = inputSet.value; 
+
+  });
+
+ // let value = 1;
+
+  //add.addEventListener('click', () => { 
+    //value = value  += 1;
+    //console.log(value)
+   // })
+
+
+
+
+
+
+//add.addEventListener('click', () => {
+//value = value  += 1
+//console.log(value);
+
+//});
+
+
+
+//output.textcontent = value;
+
+
